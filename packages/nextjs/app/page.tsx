b7d46b0eddada11c222028bdc33b04c358d5a2db
mavix21 <marcevizcarra2109@hotmail.com>
2025-11-07 12:23:45 -0300

    refactor: simplify Home component by removing unused props and integrating AddressClientWrapper.
    This eliminates the usage of a top level "use client" in the home page component, which turns the page into a server component. "use client" should only be used for interactive components.

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -1,19 +1,11 @@
-"use client";
-
 import type { NextPage } from "next";
-import { useTheme } from "next-themes";
-import { useAccount } from "wagmi";
 import { Card } from "~~/components/Card";
-import { Address } from "~~/components/scaffold-eth";
+import { AddressClientWrapper } from "~~/components/scaffold-eth/Address/AddressClientWrapper";
 import CompassIcon from "~~/icons/CompassIcon";
 import DarkBugAntIcon from "~~/icons/DarkBugAntIcon";
 import LightBugAntIcon from "~~/icons/LightBugAntIcon";
 
 const Home: NextPage = () => {
-  const { address: connectedAddress } = useAccount();
-  const { resolvedTheme } = useTheme();
-  const isDarkMode = resolvedTheme === "dark";
-
   return (
     <>
       <div className="flex items-center flex-col justify-between flex-grow pt-10">
@@ -24,57 +16,45 @@ const Home: NextPage = () => {
               <span className="block text-4xl font-bold">Scaffold-Stylus</span>
             </h1>
             <div className="flex justify-center items-center space-x-2 my-4">
-              <p className={`my-2 font-medium ${!isDarkMode ? "text-[#E3066E]" : ""}`}>Connected Address:</p>
-              <Address address={connectedAddress} />
+              <p className="my-2 font-medium text-[#E3066E] dark:text-white">Connected Address:</p>
+              <AddressClientWrapper />
             </div>
             <p className="text-center text-lg">
               Get started by editing{" "}
-              <code
-                className="italic bg-base-300 text-black text-base font-bold max-w-full break-words break-all inline-block"
-                style={{
-                  backgroundColor: isDarkMode ? "white" : "#F0F0F0",
-                }}
-              >
+              <code className="italic text-black text-base font-bold max-w-full break-words break-all inline-block bg-[#F0F0F0] dark:bg-white">
                 packages/nextjs/app/page.tsx
               </code>
             </p>
             <p className="text-center text-lg">
               Edit your smart contract{" "}
-              <code
-                className="italic bg-base-300 text-black text-base font-bold max-w-full break-words break-all inline-block"
-                style={{
-                  backgroundColor: isDarkMode ? "white" : "#F0F0F0",
-                }}
-              >
+              <code className="italic text-black text-base font-bold max-w-full break-words break-all inline-block bg-[#F0F0F0] dark:bg-white">
                 lib.rs
               </code>{" "}
               in{" "}
-              <code
-                className="italic bg-base-300 text-black text-base font-bold max-w-full break-words break-all inline-block"
-                style={{
-                  backgroundColor: isDarkMode ? "white" : "#F0F0F0",
-                }}
-              >
+              <code className="italic text-black text-base font-bold max-w-full break-words break-all inline-block bg-[#F0F0F0] dark:bg-white">
                 packages/stylus/your-contract/src
               </code>
             </p>
           </div>
         </div>
 
-        <div
-          className="h-auto sm:h-[306px] mb-3 w-full py-11"
-          style={{
-            backgroundColor: isDarkMode ? "#050505" : "white",
-          }}
-        >
+        <div className="h-auto sm:h-[306px] mb-3 w-full py-11 bg-white dark:bg-[#050505]">
           <div className="flex justify-center items-center h-full gap-12 flex-col sm:flex-row">
             {/* Debug Contracts Card */}
             <Card
-              icon={isDarkMode ? <DarkBugAntIcon /> : <LightBugAntIcon />}
+              icon={
+                <>
+                  <span className="hidden dark:block">
+                    <DarkBugAntIcon />
+                  </span>
+                  <span className="dark:hidden">
+                    <LightBugAntIcon />
+                  </span>
+                </>
+              }
               description={<>Tinker with your smart contract using the</>}
               linkHref="/debug"
               linkText="Debug Contracts"
-              isDarkMode={isDarkMode}
             />
             {/* Block Explorer Card */}
             <Card
@@ -82,7 +62,6 @@ const Home: NextPage = () => {
               description={<>Explore your local transactions with the</>}
               linkHref="/blockexplorer"
               linkText="Block Explorer"
-              isDarkMode={isDarkMode}
             />
           </div>
         </div>
